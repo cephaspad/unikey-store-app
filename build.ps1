1eb40bcd2c8117467287a85003afd7b367a65154
Cephas PAD <cephaspad@users.noreply.github.com>
2022-03-07 14:23:44 +0700

    Remove launcher (#2)
    * clean-up
    
    * added build and package
    
    * fix bug cannot run shell
    
    * fix bug invalid --Force flag
    
    * added x64 to AppManifest.xml
    
    * removed remove_launcher from workflow
    
    * change package name

---
+++ build.ps1
@@ -0,0 +1,23 @@
+$env:Path += ";C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\x64\"
+$solutionPath = $(Get-Location).Path
+$sourcePath = $solutionPath + "\src"
+$appVDrive = $sourcePath + ".\VFS\AppVPackageDrive"
+
+$unikeyReleaseRoot = "https://www.unikey.org/assets/release/"
+$unikey64FileName = "unikey43RC5-200929-win64.zip"
+# $unikey86FileName = "unikey43RC5-200929-win32.zip"
+
+Set-Location $sourcePath
+$unikeyFileName = $unikey64FileName
+Invoke-WebRequest -Uri $unikeyReleaseRoot$unikeyFileName -OutFile $unikeyFileName
+Expand-Archive -Path $unikeyFileName -DestinationPath $appVDrive -Force
+Remove-Item $unikeyFileName
+
+Set-Location $sourcePath
+MakePri new /pr . /cf "..\config\priconfig.xml"
+
+Set-Location $solutionPath
+New-Item -Path "AppPackages" -ItemType "Directory" -Force
+MakeAppX pack /d ".\src" /p ".\AppPackages\UnikeyVietnameseKeyboard.msix"
+
+Set-Location $solutionPath
